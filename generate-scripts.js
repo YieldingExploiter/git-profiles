ee56f68ec544d3e82a3c99979d7dd7af36c8d705
YieldingExploiter <YieldingExploiter@users.noreply.github.com>
2022-12-07 21:21:37 +0100

    feat: add ssh-agent shit

--- generate-scripts.js
+++ generate-scripts.js
@@ -6,16 +6,28 @@
  * this stuff is worth it, you can buy me a beer in return | YieldingExploiter
  */
 const { execSync } = require('child_process');
-const fs = require('fs'), path = require('path');
+const fs = require('fs'), path = require('path'), crypto = require('crypto');
 const template = fs.readFileSync(path.join(__dirname,'template.js'),'utf-8')
 const binDir = `${process.env.HOME ?? process.env.USERPROFILE}/.bin`;
-fs.readdirSync(path.join(__dirname,'profiles')).forEach(v=>{
+const debug = false;
+const profile = (v)=>{
   const d = JSON.parse(fs.readFileSync(path.join(__dirname,'profiles',v),'utf-8'))
+  d.socktmpname = crypto.randomBytes(64).toString('hex')
+  d.socketEncr = crypto.randomBytes(64).toString('hex')
+  
+  if (debug) {
+    d.socktmpname = Buffer.alloc(64,0).toString('hex')
+    d.socketEncr = Buffer.alloc(64,1).toString('hex')
+    d.debug=true;
+  }
+
   fs.writeFileSync(path.join(__dirname,d.exe+'.js'),template.replace(`{'config':true}`,JSON.stringify(d)))
   if (!fs.existsSync(binDir)) fs.mkdirSync(binDir)
   console.log(`compile ${d.name} (${d.exe})`);
-  execSync(`pkg -t node18 "${path.join(__dirname,d.exe+'.js')}" -o "${binDir}/${d.exe}"`, {
+  execSync(`pkg -t node18 "${path.join(__dirname,d.exe+'.js')}"${debug?` --public --no-bytecode`:''} -o "${binDir}/${d.exe}"`, {
     stdio: 'inherit'
   })
   fs.rmSync(path.join(__dirname,d.exe+'.js'))
-})
+}
+if (process.argv[2]) profile(process.argv[2])
+else fs.readdirSync(path.join(__dirname,'profiles')).forEach(v=>profile(v))
--- template.js
+++ template.js
@@ -6,9 +6,23 @@
  */
 const conf = {'config':true}
 
+const crypto = require('crypto');
+
+const isModuleInstalled = (module)=>{
+  try {
+    require.resolve(module)
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 let argIdx = 3;
 let cloneType;
 switch (process.argv[argIdx-1]) {
+  case 'init':
+    cloneType='init';
+    break;
   case 's':
   case 'sm':
   case 'sub':
@@ -26,48 +40,156 @@ switch (process.argv[argIdx-1]) {
     break;
 }
 
-const { existsSync } = require('fs');
-const path = require('path'), {execSync} = require('child_process');
-const run = execSync
-// git config init
-const initConfig = (location)=>{
-  for (const confName in conf.config) {
-    if (Object.hasOwnProperty.call(conf.config, confName)) {
-      const confVal = conf.config[confName];
-      run(`git config "${confName}" "${confVal}"`, {
-        cwd: location,
-        stdio: 'inherit'
-      })
-    }
-  }
-  console.log(`Configured git as ${conf.name}`);
-}
+const { existsSync, readFileSync, writeFileSync } = require('fs');
+const path = require('path'), {execSync,spawn} = require('child_process');
+const run = execSync;
 
-// per-arg
-const repo = process.argv[argIdx+0];
-const dir = process.argv[argIdx+1] ?? (repo ? repo.split('\\').join('/').split('/').pop() : null);
-try {
-  if (!repo) {
-    // init
-    run(`git init`, {
-      cwd: process.cwd(),
-      stdio: 'inherit'
-    })
-    initConfig(process.cwd())
-  } else {
-    // clone
-    let cloneCommand = `git ${cloneType === 'submodule' ? 'submodule add' : 'clone'} "${repo.includes(':')?'':`${conf.cloneDomain}:`}${repo.startsWith('/')?conf.repoRoute:''}${repo}" "${dir}"`
-    if (cloneType === 'clone')
-      for (const confName in conf.preConfig) {
-        if (Object.hasOwnProperty.call(conf.preConfig, confName)) {
-          const confVal = conf.preConfig[confName];
-          cloneCommand+=` --config "${confName}=${confVal}"`
+(async()=>{
+  const env = {
+    ...process.env,
+  };
+  // git config init
+  const initConfig = (location)=>{
+    for (const confName in conf.config) {
+      if (Object.hasOwnProperty.call(conf.config, confName)) {
+        const confVal = conf.config[confName];
+        run(`git config "${confName}" "${confVal}"`, {
+          cwd: location,
+          stdio: 'inherit',
+          env
+        })
+      }
+    }
+    console.log(`Configured Git as ${conf.name}`);
+  }
+
+  // find ssh agent
+  if (conf.sshKey && process.platform !== 'win32') {
+    try {
+      const authFile = process.env.PROFILE_HELPER_KEY ? `/tmp/sshagentauth-${conf.socktmpname}+${crypto.createHmac('sha512',conf.socketEncr).update(process.env.PROFILE_HELPER_KEY).digest()}` : `/tmp/sshagentauth-${process.env.USER}-${conf.socktmpname}`
+      const insecureStorageWarn = ()=>{
+        console.log(`WARN: Storing the SSH Agent\'s Authentication Socket in an insecure format.`);
+        console.log('      This is strongly discouraged. You should add the following lines to');
+        console.log('      your .zshrc/.bashrc/your shell\'s equivalent:');
+        console.log('');
+        console.log('      export PROFILE_HELPER_KEY=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 4096 | xargs)');
+        console.log('      export PROFILE_HELPER_IV=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 4096 | xargs)');
+        console.log('');
+        console.log(`      Once you\'ve done this, remove ${authFile}`);
+        console.log('      This file will automatically be removed in 15 minutes.');
+      }
+      const getEncryptionKey = ()=>process.env.PROFILE_HELPER_KEY?crypto.scryptSync(process.env.PROFILE_HELPER_KEY,conf.socketEncr):conf.socketEncr
+      const getEncryptionIv = ()=>process.env.PROFILE_HELPER_IV?crypto.scryptSync(process.env.PROFILE_HELPER_IV,conf.socketEncr):conf.socketEncr
+      const addKey = ()=>{
+        console.log('Adding Key to SSH-Agent');
+        execSync(`ssh-add -t 900 ${conf.sshKey}`, {
+          stdio: 'inherit',
+          env,
+        })
+      }
+      const ensureKey = ()=>{
+        // assume agent exists, env is populated
+        const name = execSync(`ssh-keygen -l -E sha512 -f ${conf.sshKey}`).toString('utf-8').trim();
+        const keys = execSync('ssh-add -l -E sha512').toString('utf-8').split('\n').map(v=>v.trim()).filter(v=>!!v)
+        if (!keys.includes(name))
+          addKey()
+      }
+      const spawnAgent = async ()=>{
+        const r = execSync(`ssh-agent -s`)
+        // for win32 support, this would need to def be replaced:
+        const r2 = execSync(`${r}
+echo SPLIT
+echo "{\\"SSH_AGENT_PID\\":\\"\${SSH_AGENT_PID}\\",\\"SSH_AUTH_SOCK\\":\\"\${SSH_AUTH_SOCK}\\"}"`)
+        try {
+          const dataAsStr = r2.toString().split('SPLIT').pop().trim();
+          const dataAsJSON = JSON.parse(dataAsStr)
+          for (const envKey in dataAsJSON) {
+            if (Object.hasOwnProperty.call(dataAsJSON, envKey)) {
+              const envVal = dataAsJSON[envKey];
+              env[envKey] = envVal
+            }
+          }
+          if (!process.env.PROFILE_HELPER_KEY) {
+            // for win32 support, this would def need to be replaced:
+            spawn(`zsh`, [`-c`, `sleep 900 && rm "${authFile}"`], {
+              detached:true,
+              stdio:'ignore'
+            })
+            insecureStorageWarn()
+          }
+          const encryptionKey = getEncryptionKey().toString();
+          const encryptionIv = getEncryptionIv().toString();
+          const cipher = crypto.createCipheriv('aes-256-cbc',encryptionKey.substring(0,32),encryptionIv.substring(16,32));
+          writeFileSync(authFile,Buffer.concat([cipher.update(Buffer.from(Buffer.from(dataAsStr).toString('hex'))),cipher.final()]))
+          addKey()
+        } catch (e) {
+          console.warn('WARN: SSH Agent Functionality failed with',e.toString(),'\n      Block 2');
         }
       }
-    run(cloneCommand, {
-      cwd: process.cwd(),
-      stdio: 'inherit',
-    })
-    if (existsSync(path.join(process.cwd(),dir))) initConfig(path.join(process.cwd(),dir))
+      const findSSHAgent = isModuleInstalled('ps-node') ? ()=>new Promise(resolve=>require('ps-node').lookup({command:'ssh-agent'},(err,list)=>resolve({err,list}))) : ()=>'noPSNode'//process.env.SSH_AGENT_PID
+      const sshAgentLookup = await findSSHAgent();
+      if (sshAgentLookup.err) return console.warn(err)
+      else if (sshAgentLookup === 'noPSNode') {
+        console.warn('ps-node is not installed. SSH-Agent Functionality Skipped.');
+      }else if (!sshAgentLookup.list[0]) {
+        await spawnAgent()
+        ensureKey();
+      } else if (!env.SSH_AGENT_PID || !env.SSH_AUTH_SOCK) {
+        if (existsSync(authFile)) {
+          const fileContents = readFileSync(authFile)
+          const encryptionKey = getEncryptionKey().toString();
+          const encryptionIv = getEncryptionIv().toString();
+          const decipher = crypto.createDecipheriv('aes-256-cbc',encryptionKey.substring(0,32),encryptionIv.substring(16,32));
+          let decrypted = Buffer.concat([decipher.update(fileContents),decipher.final()])
+          if (!process.env.PROFILE_HELPER_KEY)
+            insecureStorageWarn()
+          const agentEnv = (JSON.parse(Buffer.from(decrypted.toString(),'hex').toString()));
+          for (const envKey in agentEnv) {
+            if (Object.hasOwnProperty.call(agentEnv, envKey)) {
+              const envVal = agentEnv[envKey];
+              env[envKey] = envVal
+            }
+          }
+        } else await spawnAgent()
+        ensureKey()
+      } else {
+        ensureKey()
+      }
+    } catch (e) {
+      console.warn('WARN: SSH Agent Functionality failed with',e.toString(),'\n      Block 1',e);
+    }
   }
-} catch (error) {}
+
+  // per-arg
+  const repo = process.argv[argIdx+0];
+  const dir = process.argv[argIdx+1] ?? (repo ? repo.split('\\').join('/').split('/').pop() : null);
+
+  try {
+    if (!repo || cloneType === 'init') {
+      // init
+      run(`git init`, {
+        cwd: process.cwd(),
+        stdio: 'inherit',
+        env
+      })
+      initConfig(process.cwd())
+    } else {
+      // clone
+      let cloneCommand = `git ${cloneType === 'submodule' ? 'submodule add' : 'clone'} "${repo.includes(':')?'':`${conf.cloneDomain}:`}${repo.startsWith('/')?conf.repoRoute:''}${repo}" "${dir}"`
+      if (cloneType === 'clone')
+        for (const confName in conf.preConfig) {
+          if (Object.hasOwnProperty.call(conf.preConfig, confName)) {
+            const confVal = conf.preConfig[confName];
+            cloneCommand+=` --config "${confName}=${confVal}"`
+          }
+        }
+      run(cloneCommand, {
+        cwd: process.cwd(),
+        stdio: 'inherit',
+        env
+      })
+      if (existsSync(path.join(process.cwd(),dir))) initConfig(path.join(process.cwd(),dir))
+    }
+  } catch (error) {}
+  process.exit()
+})()
